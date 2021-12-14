deccc1b714cac5724e826a2cbecd254979be2c81
chara-b <chara.blgr@gmail.com>
2021-12-14 22:37:55 +0200

    filters component done

--- src/app/components/navbar/filters/filters.component.ts
+++ src/app/components/navbar/filters/filters.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
   selector: 'app-filters',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filters.component.css']
 })
 export class FiltersComponent implements OnInit {
+  @Input() location: string = ''; // this comes here from the navbar component as a property binding inside the element of the app-filters component in the html file
+  @Input() checked: boolean = false;
 
+  status = ['Working', 'New', 'Done', 'Cancelled'];
+  months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December']
+  selectedStatusOption = '';
+  selectedMonthOption = '';
+  
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,13 +8,10 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+
   checked = false;
   color: ThemePalette = 'accent';
   disabled = false;
-  status = ['Working', 'New', 'Done', 'Cancelled'];
-  months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December']
-  selectedStatusOption = '';
-  selectedMonthOption = '';
   location: string = '/todos';
 
   constructor(private route: ActivatedRoute, private router: Router) {
