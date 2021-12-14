21ce62a0eb1f4e56b497949f9ecf7aebb4894222
chara-b <chara.blgr@gmail.com>
2021-12-14 03:40:37 +0200

    first view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,17 @@ import { GridComponent } from './components/grid/grid.component';
 import { HistogramComponent } from './components/histogram/histogram.component';
 
 const routes: Routes = [
+
+
+  
+      {
+        path:'todos', component: GridComponent
+      },
+      {
+        path:'histogram', component: HistogramComponent
+      },
+    
+  
 // we have one path route here the AppComponent which has 2 children routes the GridComponent and the HistogramComponent
 // It is critical as there are views that the user should not be able to access unless they are in a particular view. This way, the structure becomes tree-like
 /* after we insert into the constructor of the parent component the route: ActivatedRoute and the router: Router we
@@ -14,7 +25,7 @@ so angular does something like a concatenation to the path string, so to do so w
 this.router.navigate(['todos'], {relativeTo:this.route}); inside corresponding method that fires on the click of a button.. where
 this.route must be the path of the parent component that is defined here as the parent of the child
 this whole thing happens because some views must be accessible only after the user has entered a specific view where the other views
-are children of this view */
+are children of this view ...
 
   {
     path:'', component: AppComponent,
@@ -27,6 +38,7 @@ are children of this view */
       },
     ]
   },
+  */
 
 ];
 
@@ -37,4 +49,4 @@ are children of this view */
 export class AppRoutingModule { }
 
 // We have also tied our parent components to an array so we do not have to import the three components everywhere we need to use them, but just import the array instead.
-export const ArrayOfParentComponents = [AppComponent, GridComponent, HistogramComponent]
+export const ArrayOfParentComponents = [GridComponent, HistogramComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppComponent } from './app.component';
 import { AppRoutingModule, ArrayOfParentComponents } from './app-routing.module';
 import { ButtonComponent } from './components/button/button.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FiltersComponent } from './components/navbar/filters/filters.component';
 import { AddNewTodoDialogComponent } from './components/navbar/add-new-todo-dialog/add-new-todo-dialog.component';
+import { HttpserviceService } from './components/httpservice.service';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
+    AppComponent,
     ButtonComponent,
     NavbarComponent,
     FiltersComponent,
@@ -18,9 +36,23 @@ import { AddNewTodoDialogComponent } from './components/navbar/add-new-todo-dial
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatPaginatorModule,
+    HttpClientModule,
+    MatDialogModule,
+    MatDatepickerModule,
+    MatTableModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    FormsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatSortModule
   ],
-  providers: [],
-  bootstrap: [ArrayOfParentComponents[0]] //ArrayOfParentComponents[0] holds our AppComponent.. this array is exported inside app-routing.module.ts
+  providers: [HttpserviceService],
+  bootstrap: [AppComponent] 
 })
 export class AppModule { }
