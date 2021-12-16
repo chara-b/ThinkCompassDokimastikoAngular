5affc9c2a862ba20348201c994e69dd87227c19a
chara-b <chara.blgr@gmail.com>
2021-12-16 08:31:09 +0200

    toolbar width fixed

--- src/app/components/histogram/histogram.component.ts
+++ src/app/components/histogram/histogram.component.ts
@@ -9,7 +9,13 @@ import { HttpserviceService } from '../httpservice.service';
 })
 export class HistogramComponent implements OnInit {
 
-  public graph: any;
+  public graph = {
+    data: [
+       // { x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: {color: 'red'} },
+        { x: [1,2,3], y: [2,6,3], type: 'histogram' },
+    ],
+    layout: {autosize: true, title: 'Todos Done Per Day'}
+  };
   todos_done:any = []
   groupedTodosOverDate_Due: any;
   dates: any = [];
